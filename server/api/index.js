2b779c016e2537a44b10be7a356a132ebcf38b8b
mahlaghaTavakoli <mahlagha.2211@gmail.com>
2025-03-22 02:53:50 +0330

    change index.js

--- server/api/index.js
+++ server/api/index.js
@@ -1,3 +1,11 @@
-const server = require("./server");
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("./db.json"); // Adjust path if needed
+const middlewares = jsonServer.defaults();
 
-export default server;
+server.use(middlewares);
+server.use(router);
+
+module.exports = (req, res) => {
+  server(req, res);
+};
